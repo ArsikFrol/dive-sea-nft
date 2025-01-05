8b43b85e4a4341fa43acc64e6b615d8666e45a5e
ArseniyFrolov <arsik.frol.555@gmail.com>
2025-01-05 13:50:49 +0500

    feat: style BlackBtn

--- src/components/UI/BlackBtn.tsx
+++ src/components/UI/BlackBtn.tsx
@@ -3,12 +3,24 @@ import styles from './styles.module.css'
 
 type Props = {
     text: string,
-    padding: string
+    padding: string,
+    bgColor?: boolean
 }
 
 const BlackBtn = React.memo(function BlackBtn(props: Props) {
     return (
-        <div style={{ padding: `${props.padding}` }} className={styles.BlackBtn}>
+        <div style={props.bgColor ?
+            {
+                border: '1.5px solid rgba(0, 0, 0, 1)',
+                backgroundColor: 'rgba(249, 249, 249, 1)',
+                padding: `${props.padding}`,
+                color: 'rgba(0, 0, 0, 1)',
+                borderRadius: '13px'
+            } : {
+                padding: `${props.padding}`,
+                borderRadius: '13px'
+            }}
+            className={styles.BlackBtn}>
             {props.text}
         </div>
     )
