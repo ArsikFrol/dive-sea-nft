b2cf98d4780dcf7cdcee693f3fa63bee1d6feae6
ArseniyFrolov <arsik.frol.555@gmail.com>
2025-01-08 14:32:50 +0500

    feat: added redux

---
+++ src/redux/reducer/allUser.js
@@ -0,0 +1,13 @@
+const defaultAllUser = {
+    user: []
+}
+
+const allUser = (state = defaultAllUser, action) => {
+    switch (action.type) {
+
+        default:
+            return { ...state }
+    }
+}
+
+export default allUser
